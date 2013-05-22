562011f67a0e55c0254b57c7e1312b30a7b7e00d
sharkithepointed <sharkithepointed@gmail.com>
2013-05-22 21:20:04 +0000

    Added some basic functionality: -SaveModel of Wavefront .obj type -Properties in both model types -Vertex list population of the DMC4 .mod type

--- DMC4ModelConverter/DMC4ModelConverter/MTFramework/DMC4Model.cs
+++ DMC4ModelConverter/DMC4ModelConverter/MTFramework/DMC4Model.cs
@@ -10,6 +10,11 @@ namespace DMC4ModelConverter.MTFramework
 {
     public class DMC4Model : IModel
     {
+        //const addresses
+        private const int HEADADDR_ADDR_VERT_START = 0x3C;
+        private const int HEADADDR_ADDR_TRI_START = 0X44;
+        private int ADDR_VERT_START;
+        private int ADDR_TRI_START;
         public DefaultVertexDeclaration[] Vertices { get; set; }
 
         public DMC4Model()
@@ -26,7 +31,27 @@ namespace DMC4ModelConverter.MTFramework
             string s = Encoding.UTF8.GetString(filetype, 0, filetype.Length);
             if (s == "MOD")//This is probably the correct file
             {
+                //**************
+                // Parse header
+                //**************
+                ADDR_VERT_START = BitConverter.ToInt32(data.Skip(HEADADDR_ADDR_VERT_START).Take(4).ToArray(),0);
+                ADDR_TRI_START = BitConverter.ToInt32(data.Skip(HEADADDR_ADDR_TRI_START).Take(4).ToArray(), 0);
 
+                //************
+                // Parse data
+                //************
+                if((ADDR_VERT_START - ADDR_TRI_START)%32 == 0)
+                {
+                    int vertexCount = (ADDR_VERT_START - ADDR_TRI_START) / 32;
+                    //Create vertices
+                    Vertices = new DefaultVertexDeclaration[vertexCount];
+                    for (int i = 0; i < vertexCount; i++)
+                    {
+                        int address = ADDR_VERT_START + i * 32;
+                        DefaultVertexDeclaration vertex = DefaultVertexDeclaration.FromByteCode(data.Skip(address).Take(32).ToArray());
+                        Vertices[i] = vertex;
+                    }
+                }
 
             }
         }
