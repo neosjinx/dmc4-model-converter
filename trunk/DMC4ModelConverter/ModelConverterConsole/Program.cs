84e7551798ffbc74a27af58d8f2b9e7a44fd4e05
sharkithepointed@gmail.com <sharkithepointed@gmail.com@2731477a-169b-efb6-25ac-5bb3d8baba91>
2013-05-23 21:08:50 +0000

    Changed Project Type to library and added a test application project.

---
+++ trunk/DMC4ModelConverter/ModelConverterConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMC4ModelConverter;
+using DMC4ModelConverter.MTFramework;
+using DMC4ModelConverter.Wavefront;
+
+namespace ModelConverterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(">");
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                if (input.StartsWith("export "))
+                {
+                    string[] parameters = input.Split(new char[] { ' ' });
+                    if (parameters.Length == 3)
+                    {
+                        ExportRoutine(parameters[1], parameters[2]);
+                    }
+                }
+                Console.Write(">");
+                input = Console.ReadLine();
+            }
+        }
+
+        static void ExportRoutine(string sourceFile,string targetFile)
+        {
+            //Load src file
+            DMC4Model mdl = new DMC4Model();
+            mdl.LoadModel(sourceFile);
+
+            //Convert
+            WavefrontModel convertedMdl = WavefrontModel.FromDMC4Model(mdl);
+            convertedMdl.SaveModel(targetFile);
+        }
+    }
+}
